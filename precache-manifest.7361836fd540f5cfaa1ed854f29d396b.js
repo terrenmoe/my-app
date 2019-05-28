eccaeeb9579468df6ac450e20226e930eeb7cdff
Alex Rossiter <terrenmoe@gmail.com>
2019-05-27 17:24:15 -0700

    Updates

---
+++ precache-manifest.7361836fd540f5cfaa1ed854f29d396b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44df716598944b901efb01557ca42bef",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ef1b782f44e91e8170cf",
+    "url": "/my-app/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/my-app/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "ef1b782f44e91e8170cf",
+    "url": "/my-app/static/js/main.0f053146.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
